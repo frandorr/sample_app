25338363e56098786db3c083b8b2fc50f723613d
frandorr <fran.dorr@gmail.com>
2013-09-17 11:39:46 -0300

    add title base by default

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title) 
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty? #if there's no page title, it uses base_title
+      base_title
+    else #otherwise it adds | and page title using string interpolation
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
