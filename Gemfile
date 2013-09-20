696a323fbb95a42e211e94288da233153046e1e9
frandorr <fran.dorr@gmail.com>
2013-09-20 20:16:47 -0300

    Add has_secure_pass

--- Gemfile
+++ Gemfile
@@ -4,7 +4,9 @@ ruby '2.0.0'
 
 gem 'rails', '4.0.0'
 
-gem 'bootstrap-sass', '2.3.2.0' #bootstrap uses Less CSS by default,this gem converts it to sass
+gem 'bootstrap-sass', '2.3.2.0' #bootstrap uses Less CSS by default,
+                                #this gem converts it to sass
+gem 'bcrypt-ruby', '3.0.1' #irreversibly encript pass to pasword hash
 
 group :development, :test do
   gem 'sqlite3', '1.3.7'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ActiveRecord::Base
+	#before_save callback. Emails downcase to avoid problems with database
+	before_save { self.email = email.downcase }	
+
+	validates :name,  presence: true, length: { maximum: 50 }
+	# /i = case insensitive, \A = begining of string, \z = match end of string
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]*[a-z\d\-]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+  									uniqueness: { case_sensitive: false } 
+
+  	#password:
+  	has_secure_password
+
 end
