0f324232a6e1531360875a2d2edf231b5c8f8caf
frandorr <fran.dorr@gmail.com>
2013-09-25 14:01:06 -0300

    msg

--- Push-Git-Heroku.sh
+++ Push-Git-Heroku.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-rake assets:precompile
+echo "Do you want ONLY to commit? y/N"
+read cans
+echo "Have you change statics (CSS, etc)? y/N"
+read ans
+if $ans == "y"; then
+	echo "Raking precompiled assets..\n"
+	rake assets:precompile
+fi
 git add .
-git commit -m "Add precompiled assets for Heroku"
-git co master
-git push
-git push heroku master
+echo "Enter commit message: "
+read msg
+git commit -m msg
+echo "Commited done\n"
+if $cans == "N"; then
+	echo "Checkout master..."
+	git co master
+	echo "Pushing into github"
+	git push
+	echo "Pushing into heroku"
+	git push heroku master
+fi
