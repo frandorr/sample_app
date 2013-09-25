9a2e8039cfb4671c3e29be2806e12c6e01489447
frandorr <fran.dorr@gmail.com>
2013-09-25 14:13:40 -0300

    msg

--- Push-Git-Heroku.sh
+++ Push-Git-Heroku.sh
@@ -4,7 +4,7 @@ echo "Do you want ONLY to commit? y/N"
 read cans
 echo "Have you change statics (CSS, etc)? y/N"
 read ans
-if ans == "y"; then
+if "$ans" == "y"; then
 	echo "Raking precompiled assets..\n"
 	rake assets:precompile
 fi
@@ -13,7 +13,7 @@ echo "Enter commit message: "
 read msg
 git commit -m msg
 echo "Commited done\n"
-if cans == "N"; then
+if "$cans" == "N"; then
 	echo "Checkout master..."
 	git co master
 	echo "Pushing into github"
