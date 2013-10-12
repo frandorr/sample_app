65571abaf5c32358256804f39864be55e61872f2
frandorr <fran.dorr@gmail.com>
2013-10-12 09:50:57 -0300

    algunos tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,6 +35,8 @@ describe "Authentication" do
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
+        it { should_not have_link('Settings') }
+        it { should_not have_link('Profile') }
       end
     end
 
