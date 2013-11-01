b63156ddbbb9c8acbea5aa86546334e63f0dca5a
frandorr <fran.dorr@gmail.com>
2013-11-01 16:39:01 -0300

    cambio titulo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Returns the full title on a per-page basis.
   def full_title(page_title) 
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Calo: your adventure starts here"
     if page_title.empty? #if there's no page title, it uses base_title
       base_title
     else #otherwise it adds | and page title using string interpolation
